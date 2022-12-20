530f414fea11e192b8d4f990bdb1a93fcf91966b
ZoOL <zhooul@gmail.com>
2022-12-20 12:35:20 +0800

    Update MenuPlugin

--- src/menu.rs
+++ src/menu.rs
@@ -1,11 +1,33 @@
 use crate::GameState;
+use bevy::app::AppExit;
 use bevy::prelude::*;
 
 pub struct MenuPlugin;
 
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system_set(SystemSet::on_enter(GameState::Menu).with_system(main_menu_setup));
+        // At start, the menu is not enabled. This will be changed in `menu_setup` when
+        // entering the `GameState::Menu` state.
+        // Current screen in the menu is handled by an independent state from `GameState`
+        app.add_state(MenuState::Main)
+            .add_system_set(SystemSet::on_enter(GameState::Menu).with_system(menu_setup))
+            .add_system_set(SystemSet::on_enter(MenuState::Main).with_system(main_menu_setup))
+            .add_system_set(
+                SystemSet::on_exit(MenuState::Main).with_system(despawn_screen::<OnMainMenuScreen>),
+            )
+            // Systems to handle the settings menu screen
+            .add_system_set(
+                SystemSet::on_enter(MenuState::Settings).with_system(settings_menu_setup),
+            )
+            .add_system_set(
+                SystemSet::on_exit(MenuState::Settings)
+                    .with_system(despawn_screen::<OnSettingsMenuScreen>),
+            )
+            .add_system_set(
+                SystemSet::on_update(GameState::Menu)
+                    .with_system(menu_action)
+                    .with_system(button_system),
+            );
     }
 }
 
@@ -19,9 +41,25 @@ enum MenuButtonAction {
     Quit,
 }
 
+// State used for the current menu screen
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+enum MenuState {
+    Main,
+    Settings,
+    SettingsDisplay,
+    Disabled,
+}
+
 // Tag component used to tag entities added on the main menu screen
 #[derive(Component)]
 struct OnMainMenuScreen;
+
+#[derive(Component)]
+struct OnSettingsMenuScreen;
+
+// Tag component used to mark wich setting is currently selected
+#[derive(Component)]
+struct SelectedOption;
 
 const TEXT_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 
@@ -29,6 +67,10 @@ const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
 const HOVERED_PRESSED_BUTTON: Color = Color::rgb(0.25, 0.65, 0.25);
 const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+fn menu_setup(mut menu_state: ResMut<State<MenuState>>) {
+    let _ = menu_state.set(MenuState::Main);
+}
 
 fn main_menu_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let font = asset_server.load("fonts/FiraSans-Bold.ttf");
@@ -155,4 +197,108 @@ fn main_menu_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     parent.spawn(TextBundle::from_section("Quit", button_text_style));
                 });
         });
+}
+
+fn settings_menu_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let button_style = Style {
+        size: Size::new(Val::Px(200.0), Val::Px(65.0)),
+        margin: UiRect::all(Val::Px(20.0)),
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        ..default()
+    };
+
+    let button_text_style = TextStyle {
+        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+        font_size: 40.0,
+        color: TEXT_COLOR,
+    };
+
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    margin: UiRect::all(Val::Auto),
+                    flex_direction: FlexDirection::Column,
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                background_color: Color::CRIMSON.into(),
+                ..default()
+            },
+            OnSettingsMenuScreen,
+        ))
+        .with_children(|parent| {
+            for (action, text) in [
+                (MenuButtonAction::SettingsDisplay, "Display"),
+                (MenuButtonAction::BackToMainMenu, "Back"),
+            ] {
+                parent
+                    .spawn((
+                        ButtonBundle {
+                            style: button_style.clone(),
+                            background_color: NORMAL_BUTTON.into(),
+                            ..default()
+                        },
+                        action,
+                    ))
+                    .with_children(|parent| {
+                        parent.spawn(TextBundle::from_section(text, button_text_style.clone()));
+                    });
+            }
+        });
+}
+
+// Generic system that takes a component as a parameter, and will despawn all entities with that component
+fn despawn_screen<T: Component>(to_despawn: Query<Entity, With<T>>, mut commands: Commands) {
+    for entity in &to_despawn {
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
+fn menu_action(
+    interaction_query: Query<
+        (&Interaction, &MenuButtonAction),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut app_exit_events: EventWriter<AppExit>,
+    mut menu_state: ResMut<State<MenuState>>,
+    mut game_state: ResMut<State<GameState>>,
+) {
+    for (interaction, menu_button_action) in &interaction_query {
+        if *interaction == Interaction::Clicked {
+            match menu_button_action {
+                MenuButtonAction::Quit => app_exit_events.send(AppExit),
+                MenuButtonAction::Play => {
+                    game_state.set(GameState::Playing).unwrap();
+                    menu_state.set(MenuState::Disabled).unwrap();
+                }
+                MenuButtonAction::Settings => menu_state.set(MenuState::Settings).unwrap(),
+                MenuButtonAction::SettingsDisplay => {
+                    menu_state.set(MenuState::SettingsDisplay).unwrap();
+                }
+                MenuButtonAction::BackToMainMenu => menu_state.set(MenuState::Main).unwrap(),
+                MenuButtonAction::BackToSettings => {
+                    menu_state.set(MenuState::Settings).unwrap();
+                }
+            }
+        }
+    }
+}
+
+// This system handles changing all buttons color based on mouse interaction
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor, Option<&SelectedOption>),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color, selected) in &mut interaction_query {
+        *color = match (*interaction, selected) {
+            (Interaction::Clicked, _) | (Interaction::None, Some(_)) => PRESSED_BUTTON.into(),
+            (Interaction::Hovered, Some(_)) => HOVERED_PRESSED_BUTTON.into(),
+            (Interaction::Hovered, None) => HOVERED_BUTTON.into(),
+            (Interaction::None, None) => NORMAL_BUTTON.into(),
+        }
+    }
 }
